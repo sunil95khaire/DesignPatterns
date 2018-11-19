ce7812bde17c110f5fae0cde2d447b62b1c2f637
Sunil Khaire <skhaire@affinionds.com>
2018-11-19 11:31:59 +0530

    Adapter class changes

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Adapter
 {
+    // Adapter Design Pattern
     class Program
     {
         static void Main(string[] args)
